a13b12fdf1bd49347dadfc92a4281a0c77a1c995
Dimitar Nizamov <dimitur2204@gmail.com>
2025-04-03 10:19:31 +0200

    add proper route basename

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,7 +4,7 @@ import PokemonDetails from "../components/PokemonDetails";
 import About from "../pages/About";
 
 const AppRouter = () => (
-    <Router basename="/WEB2">
+    <Router basename="/pokedex">
         <Routes>
             <Route path="/" element={<Pokedex />} />
             <Route path="/pokemon/:id" element={<PokemonDetails />} />
